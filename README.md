cc485261ea9759dec1dc89bfa5dd28420852a545
Mike Croft <backtotheoldhouse@gmail.com>
2015-09-20 20:52:40 +0100

    starts EC2 instance, waits for Jenkins to be up, then starts build.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Dependencies
 
 * Boto3
+    * needs a config file (~/.aws/config) with `aws_access_key_id`, `aws_secret_access_key` and `region` set under `[default]`
 * pygit2
--- build-payara.py
+++ build-payara.py
@@ -2,13 +2,49 @@
 
 import boto3.ec2
 import configparser
+import json
+import urllib
+import jenkins
 
-secrets = configparser.ConfigParser()
-secrets.read('.secrets.ini')
 
-print(secrets['AWS_SECRET_ACCESS_KEY'])
+ec2 = boto3.resource('ec2')
+jenkinsIP = '0.0.0.0'
 
-conn = boto3.ec2.connect_to_region("eu-west-1", aws_access_key_id=secrets['AWS_ACCESS_KEY_ID'], aws_secret_access_key=secrets['AWS_SECRET_ACCESS_KEY'])
+def startInstance(instname):
+    global jenkinsIP
+    inst = ec2.Instance(instname)
+    print(json.dumps(inst.start(), indent=4))
+    jenkinsIP = inst.public_ip_address
+    print(jenkinsIP)
 
-jenkins = conn.get_all_instances(instance_ids=['i-f5095b58'])
-print(jenkins.start())
+def stopInstance(instname):
+    inst = ec2.Instance(instname)
+    print(json.dumps(inst.stop(), indent=4))
+
+def isServiceUp(URL):
+    conn = urllib.request.urlopen(URL)
+    return (conn.getcode() == 200)
+
+
+
+def buildJob(jobName):
+    svr = jenkins.Jenkins('http://' + jenkinsIP + ':8080')
+    print(svr.build_job(jobName))
+    print(svr.get_job_config(jobName))
+
+
+
+
+
+
+
+startInstance('i-f5095b58')
+while (!(isServiceUp('http://' + jenkinsIP + ':8080'))):
+    time.sleep(2)
+
+buildJob('Payara')
+
+
+#stopInstance('i-f5095b58')
+
+
